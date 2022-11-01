b53ff1a1825f1eafde1f56f2d744e1d70d285c8c
fumi <fumi01200924@gmail.com>
2022-11-02 00:06:10 +0900

    initial

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm fpga-load.dtb
+mkdir /config/device-tree/overlays/fpga
+dtc -I dts -O dtb -o fpga-load.dtb fpga-load.dts
+cp fpga-load.dtb /config/device-tree/overlays/fpga/dtbo
+
+rm fclk0-zynqmp.dtb
+mkdir /config/device-tree/overlays/fclk0
+dtc -I dts -O dtb -o fclk0-zynqmp.dtb fclk0-zynqmp.dts
+cp fclk0-zynqmp.dtb /config/device-tree/overlays/fclk0/dtbo
+
+mkdir /config/device-tree/overlays/axigpio-uio
+dtc -I dts -O dtb -o axigpio-uio.dtb axigpio-uio.dts
+cp axigpio-uio.dtb /config/device-tree/overlays/axigpio-uio/dtbo
---
+++ rminit.sh
@@ -0,0 +1,3 @@
+rmdir /config/device-tree/overlays/axigpio-uio
+rmdir /config/device-tree/overlays/fclk0
+rmdir /config/device-tree/overlays/fpga
